952e884f6d20ea156d5d4f64b1dfe588fb7b3411
Marc-Sweeney <60797702+Marc-Sweeney@users.noreply.github.com>
2020-04-25 18:16:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+$(".right").backstretch("truckonmotorway.jpeg");
+
+$(".count").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text()
+      },
+      {
+        duration: 4000,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+      }
+    );
+});
+
+
+
